b62a2cfe1af6f04ebab1c1dabca7046774850fa0
Malik-Zohaib-Debug <zohaibzahid112233@gmail.com>
2023-02-15 21:42:37 +0500

    more questions

---
+++ q13.ts
@@ -0,0 +1,10 @@
+//Guest List: If you could invite anyone, living or deceased, to dinner, who would you 
+//invite? Make a list that includes at least three people you’d like to invite to dinner. 
+//Then use your list to print a message to each person, inviting them to dinner.
+
+var invitedPersons:string [];
+invitedPersons = ["faizan", "abdullah", "shan"];
+
+for(var i:number = 0; i<invitedPersons.length; i++){
+    console.log(invitedPersons[i] + " You are invited to dinner");
+}
