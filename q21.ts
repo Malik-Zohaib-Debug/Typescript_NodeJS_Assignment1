b3c01226d19ce7241884970dde33c9f2e0a86081
Malik Zohaib Zahid <zohaibzahid112233@gmail.com>
2023-02-20 21:52:19 +0500

    q21

---
+++ q21.ts
@@ -0,0 +1,8 @@
+//Intentional Error: If you haven’t received an array index error in one 
+//of your programs yet, try to make one happen. Change an index in one 
+//of your programs to produce an index error. Make sure you correct the 
+//error before closing the program.
+
+//list=[65,"alex",'dog']
+console.log("Intentional Error calling for del element at -4 which is out of range hehehehehe........")
+//delete list[-4]
